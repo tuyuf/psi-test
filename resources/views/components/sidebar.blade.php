afd682fb5681e064642e8cb8dfa37586ed0d0a51
Fayyadh Muhammad Habibie <habibie@f-2.local>
2026-08-11 14:12:04 +0700

    fix: ganti sidebar brand MHSC dengan logo JAPSI

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,10 +3,7 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink "></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">MHSC</div>
+        <img src="{{ asset('img/Logo-JAPSI.png?v=2') }}" alt="Logo JAPSI" style="max-height: 50px; width: auto;">
     </a>
 
     <!-- Divider -->
